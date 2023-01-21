1b40ed65e19f825c58f5fccbd183f5413629d1e8
pierocarrion <piero.january15@gmail.com>
2023-01-20 23:41:33 -0500

    Final project

--- index.js
+++ index.js
@@ -1,18 +1,86 @@
 const express = require('express')
-const app = express()
 const cors = require('cors')
+const bp = require('body-parser')
+
+const app = express()
+
 require('dotenv').config()
 
 app.use(cors())
+app.use(bp.json())
+app.use(bp.urlencoded({ extended: true }))
 app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
 
-
-
-
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
+})
+
+const users = []
+const exercises = []
+
+app.post('/api/users', (req, res) =>{
+  const username = req.body.username
+  const _id = users.length
+  const user = {
+    _id,
+    username,
+  }
+  users.push(user)
+  res.json(user)
+})
+
+app.get('/api/users', (req,res) =>{
+  res.json(users)
+})
+
+app.post('/api/users/:_id/exercises', (req, res) =>{
+  const description = req.body.description
+  const duration = req.body.duration
+  const date = req.body?.date || new Date()
+  const _id = req.params._id
+
+  const username = users.find(({_id})=> _id === _id)?.username
+
+  const exercise = {
+    _id,
+    description,
+    duration,
+    date
+  }
+
+  exercises.push(exercise)
+
+  res.json({ ...exercise, username})
+})
+
+app.get('/api/users/:_id/logs/:from?/:to?/:limit?', (req,res) =>{
+  const id = req.params._id
+  //Optional params
+  const from = req.query.from ?? new Date(-8640000000000000);
+  const to = req.query.to ?? new Date(8640000000000000);
+  const limit = req.query.limit
+
+  const username = users.find(({_id}) => _id == id)?.username
+
+  const log = exercises
+    .filter(({_id, date})=> _id === id && date >= from && date <= to)
+    .slice(0, limit)
+    .map(({description, duration, date}) => ({
+      description,
+      duration: Number(duration),
+      date: date.toDateString(),
+    }))
+    
+  const result = {
+    username,
+    count: log.length,
+    _id: id,
+    log
+  }
+  
+  res.json(result)
 })
